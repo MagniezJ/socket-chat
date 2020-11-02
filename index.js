0cc4c4be6eb0bfbb1a7dfdc47c4b586863ec542e
Ju'manji <67460476+MagniezJ@users.noreply.github.com>
2020-11-02 11:38:57 +0100

    Socket chat

---
+++ index.js
@@ -0,0 +1,24 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.sockets.on('connection', function (socket, pseudo) {
+    
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function(pseudo) {
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+        console.log(socket.pseudo+":"+message)
+    }); 
+});
+
+http.listen(3000, () => {})
